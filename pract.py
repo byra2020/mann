f5d21bcfe7c9258f2ec2c193464f083f471bcac7
yadamjavdelger-source <yadamjavdelger@gmail.com>
2026-03-18 10:52:36 +0800

    Lol

--- pract.py
+++ pract.py
@@ -1,8 +1,12 @@
 print("Hello, bayar")
-<<<<<<< HEAD
+
 print("hello,delger")
 print("hooy")
 
-=======
+
 print("asdasdsafnhsiod")
->>>>>>> 8020f7023afb86c4884e36939f6a88d9f489fbec
+print("asdasdsafnhsiod")
+print("asdasdsafnhsiod")
+print("asdasdsafnhsiod")
+print("asdasdsafnhsiod")
+print("asdasdsafnhsiod")
